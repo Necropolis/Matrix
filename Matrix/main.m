35861b99a85b50c7fbdde32c4e9e0a9a86df39e0
Chris <lordsauronthegreat@gmail.com>
2011-12-16 09:20:21 -0700

    Initial Commit

---
+++ Matrix/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Matrix
+//
+//  Created by Christopher Miller on 12/16/11.
+//  Copyright (c) 2011 FSDEV. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
