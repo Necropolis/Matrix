702a7f976ee05392944d91794d1f7c2072ad34bf
Chris <lordsauronthegreat@gmail.com>
2011-12-16 09:50:21 -0700

    Docrumentations?

--- Matrix/FSMatrix.h
+++ Matrix/FSMatrix.h
@@ -11,7 +11,38 @@
 /**
  * Simple two-dimensional container utilizing `NSArray` under the hood.
  * 
- * This container emulates the class-cluster that is `NSArray` and its mutable counterpart, `NSMutableArray`.
+ * `FSMatrix` differs from a standard two-dimensional array graph because it enforces a constant row-width. Every row is guaranteed to have as many columns as the longest row. So for the following `FSMatrix` the assertion is true:
+ *
+ *     FSMatrix* deadFolks = [[FSMatrix alloc] initWithRows:
+ *       [NSArray arrayWithObjects:
+ *         @"Franklin Roosevelt",
+ *         @"Lyndon Johnson", nil],
+ *       [NSArray arrayWithObjects:
+ *         @"Josef Stalin",
+ *         @"Leon Trotsky",
+ *         @"Vladimir Lenin", nil],
+ *       nil];
+ *     NSAssert([deadFolks objectForColumn:2 row:0]==[NSNull null],
+ *              @"Assertion Failure!"); // this will always be true,
+ *     // so the assertion will never fail
+ *
+ * You can use the `lambda` parameter of some construtors to control what the matrix will be prepopulated with. For example, if I were building a video game and wanted an isometric map to hold all the actors, I'd want a matrix of `NSMutableSet` objects to hold everything. The best initializer to use in that situation is:
+ *
+ *     NSUInteger cols = ...; // hopefully you'll get these from
+ *     NSUInteger rows = ...; // somewhere...
+ *     FSMatrix* map =
+ *       [[FSMatrix alloc]
+ *         initWithColumns:cols
+ *                    rows:rows
+ *                  lambda:id(^)(){
+ *                    return [NSMutableSet setWithCapacity:10];
+ *                   }];
+ *
+ * Each and every value in the matrix is a mutable set, ready for action.
+ *
+ * This container emulates the class-cluster that is `NSArray`. There is no `FSMatrix` class that you will use; rather, you'll be using `FSMatrixImpl` (which is returned by calls to `alloc` on `FSMatrix`). `FSMatrixImpl` uses `NSArray` internally, whereas `FSMutableMatrix` uses `NSMutableArray`. When copying an `FSMutableMatrix` (not making a mutable copy!) the internal data structures are deep-copied to immutable variants.
+ *
+ * The entire project requires Apple LLVM 3.0 Automatic Reference Counting.
  */
 @interface FSMatrix : NSObject <NSCopying, NSMutableCopying, NSCoding>
 
